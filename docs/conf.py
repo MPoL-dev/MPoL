9351389e6213cbced830e4a3a865a4ec9aafdeed
Ian Czekala <iancze@gmail.com>
2019-12-26 18:35:34 -0800

    seeing if we can circumvent memory issue with system_packages

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,6 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -72,6 +71,8 @@ html_theme = "sphinx_rtd_theme"
 #         "donate.html",
 #     ]
 # }
+
+master_doc = "index"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
