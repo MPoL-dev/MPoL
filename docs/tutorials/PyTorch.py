4c6ebace06887e1e8c478da881263799972f4c53
Hannah Grzybowski <35509654+hgrzy@users.noreply.github.com>
2021-06-01 17:56:20 -0400

    Automated grad descent, uniform eqns, note chain rule
    Automated gradient descent processes instead of hard coded data. Made equations of uniform formatting. Added note about mechanistic application of chain rule in Calculating Gradients section

--- docs/tutorials/PyTorch.py
+++ docs/tutorials/PyTorch.py
@@ -77,14 +77,14 @@ y = x ** 2
 print(f"y: {y}")
 print(f"x: {x}")
 
-# But what if we wanted to calculate the gradient of $y$ with respect to $x$? Using calculus, we find that the answer is $\frac{dy}{dx}$ = $2*x$. The derivative evaluated at $x$=$3$ is $6$.
+# But what if we wanted to calculate the gradient of $y$ with respect to $x$? Using calculus, we find that the answer is $\frac{dy}{dx} = 2*x$. The derivative evaluated at $x = 3$ is $6$.
 #
 # We can see if PyTorch gets the same answer as us if we do:
 
 y.backward()  # populates gradient (.grad) attributes of y with respect to all of its independent variables
 x.grad  # returns the grad attribute (the gradient) of y with respect to x
 
-# PyTorch uses the concept of automatic differentiation to calculate the derivative. Instead of computing the derivative as we would by hand, the program is using a computational graph.
+# PyTorch uses the concept of automatic differentiation to calculate the derivative. Instead of computing the derivative as we would by hand, the program is using a computational graph. This is a mechanistic application of the chain rule. For example, a tree with several operations on $x$ resulting in a final output $y$ will use the chain rule to compute the differential associated with each operation and multiply these differentials together to get the derivative of y with respect to x.
 
 # ## Optimizing a Function with Gradient Descent
 #
@@ -94,30 +94,42 @@ x.grad  # returns the grad attribute (the gradient) of y with respect to x
 #
 # One other thing we'd have to consider is our step size. If we take very small steps in the direction of the descent, it will take us a longer time than if we take larger steps. However, if we take super long steps, we might completely miss the flat part of the valley, and start ascending the other side of the valley.
 
-# We can look at the gradient descent from a more mathematical lense by looking at the graph $z$ = $t^2$:
+# We can look at the gradient descent from a more mathematical lense by looking at the graph $y = x^2$:
 
-t = np.linspace(-5, 5, 100)
-plt.plot(t, t ** 2)
+# +
+def y(x_input):
+    y = x_input ** 2
+    return y
 
-# We start on the left hill at the point (-4, 16):
 
-t = np.linspace(-5, 5, 100)
-plt.plot(t, t ** 2)
-plt.scatter(-4, 16)
+x = np.linspace(-5, 5, 100)
+plt.plot(x, y(x))  # plot y = x ** 2
+# -
+
+# We will choose to start on the left hill at the point (-4, 16). This is an arbitrary choice, any point could've been chosen as the start:
+
+# +
+x = np.linspace(-5, 5, 100)
+plt.plot(x, y(x))  # plot y = x ** 2
+
+x_start = -4  # x coordinate of starting point
+y_start = 16  # y coordinate of starting point
+
+plt.scatter(x_start, y_start)  # plot starting point
 plt.show()
+# -
 
 # If we plot a tangent line at this point, we see which directions we can go:
 
 # +
-t = np.linspace(-5, 5, 100)
-plt.plot(t, t ** 2)
+x = np.linspace(-5, 5, 100)
+plt.plot(x, y(x))  # plot y = x ** 2
 
 
-plt.scatter(-4, 16)  # point
-tp = -4
-yp = 16
+plt.scatter(x_start, y_start)  # plot starting point
 
-plt.plot(t, tp * 2 * (t - tp) + yp)  # tangent line
+slope_start = 2 * x_start  # derivative of y = x ** 2 evaluated at x_start
+plt.plot(x, slope_start * (x - x_start) + y_start)  # tangent line
 
 plt.xlim(xmin=-5, xmax=5)
 plt.ylim(ymin=0, ymax=25)
@@ -129,26 +141,54 @@ plt.show()
 # $x_{new} = x_{current} - \nabla y(x_{current}) * (step \: size)$
 #
 # where:
+#
 # - $x_{current}$ is our current x value
 #
 # - $\nabla y(x_{current})$ is the gradient at our current point
 #
-# - $(step \: size)$ is a value we choose that tells us scales our steps
+# - $(step \: size)$ is a value we choose that scales our steps
 #
 # This makes sense because we start at some x value and we want to get to a new x value that is closer to the minimum. In the above plot, the gradient is negative. If we were to add the gradient to our current x value that would bring us further away from the minimum to a more negative x value. This is because the gradient points in the direction of the steepest ascent while we are looking to go in the direction of the steepest descent. This is why there is a negative in the equation.
 #
 #
-# We will choose `step size = 0.1`:
+# We will choose <code> step_size = 0.1 </code>:
 
 # +
-t = np.linspace(-5, 5, 100)
-plt.plot(t, t ** 2)
+x = np.linspace(-5, 5, 100)
+plt.plot(x, y(x))  # plot y = x ** 2
+
+# We chose step size of 0.1
+step_size = 0.1
+
+# Define a function to get slope of y = x ** 2 at any x value:
+def dy_dx(x_input):
+    gradient = 2 * x_input  # derivative of y = x ** 2
+    return gradient
 
 
-plt.scatter(-4, 16)  # point
+# Current values at starting point we chose:
+x_current = x_start
+y_current = y_start
+dy_dx_current = dy_dx(x_current)
 
-plt.scatter(-3.2, 10.24)  # second point
-plt.arrow(-4, 16, 0.8, -5.76, width=0.07, color="r")
+# To keep track of our coordinates at each step, we will create 2 lists, initialized with the values at our chosen starting point
+x_coords = [x_current]
+y_coords = [y_current]
+
+
+# Using equation for x_new to get second point
+x_new = x_current - dy_dx(x_current) * step_size
+
+# Plug in x_new into y = x ** 2 to get y_new of second point
+y_new = y(x_new)
+
+
+# Store second point coordinates in our lists
+x_coords.append(x_new)
+y_coords.append(y_new)
+
+
+plt.scatter(x_coords, y_coords)  # plot points showing steps
 plt.text(-4.5, 12, "step 1")
 
 plt.xlim(xmin=-5, xmax=5)
@@ -156,104 +196,66 @@ plt.ylim(ymin=0, ymax=25)
 plt.show()
 # -
 
-# The gradient at our new point is still not close to zero, meaning we haven't reached the minimum. So we will take another step:
+# The gradient at our new point is still not close to zero, meaning we haven't reached the minimum. We continue this process of checking if the gradient is nearly zero, and taking a step in the direction of steepest descent until we reach the bottom of the valley. We'll say we've reached the bottom of the valley when the absolute value of the gradient is $<0.1$:
 
 # +
-t = np.linspace(-5, 5, 100)
-plt.plot(t, t ** 2)
+x = np.linspace(-5, 5, 100)
+plt.plot(x, y(x))  # plot y = x ** 2
+
+# We are now at our second point so we need to update our current values
+x_current = x_new
+y_current = y_new
 
 
-plt.scatter(-4, 16)  # point
+# We automate this process with the following while loop
+while abs(dy_dx(x_current)) >= 0.1:  # Check to see if we're at minimum
+    # Get new coordinates
+    x_new = x_current - dy_dx(x_current) * step_size
+    y_new = y(x_new)
 
-plt.scatter(-3.2, 10.24)  # second point
-plt.arrow(-4, 16, 0.8, -5.76, width=0.07, color="r")
-plt.text(-4.5, 12, "step 1")
+    # Add new coordinates to lists
+    x_coords.append(x_new)
+    y_coords.append(y_new)
 
-plt.scatter(-2.56, 6.5536)  # third point
-plt.arrow(-3.2, 10.24, 0.64, -3.6864, width=0.07, color="r")
-plt.text(-4, 7, "step 2")
+    # Update current position
+    x_current = x_new
 
+
+plt.scatter(x_coords, y_coords)  # plot points showing steps
 
 plt.xlim(xmin=-5, xmax=5)
 plt.ylim(ymin=0, ymax=25)
 plt.show()
 # -
 
-# We would continue this process of checking if the gradient is nearly zero, and taking a step in the direction of steepest descent until we reach the bottom of the valley. We'll say we've reached the bottom of the valley when the absolute value of the gradient is $<0.1$:
+# This works, but it takes a long time since we have several small steps. We could speed up the process by taking large steps.  We're only focused on the affects of changing the step size, so we will keep (-4, 16) as the starting point and increase the step size to $1.5$. Our first step now looks like:
 
 # +
-t = np.linspace(-5, 5, 100)
-plt.plot(t, t ** 2)
+x_large_step = np.linspace(-20, 20, 1000)
+plt.plot(x_large_step, y(x_large_step))  # plot y = x ** 2
 
-x_vals = np.array(
-    [
-        -4,
-        -3.2,
-        -2.56,
-        -2.048,
-        -1.6384,
-        -1.31072,
-        -1.048576,
-        -0.8388608,
-        -0.67108864,
-        -0.536870912,
-        -0.42949673,
-        -0.343597384,
-        -0.274877907,
-        -0.219902326,
-        -0.17592186,
-        -0.140737488,
-        -0.112589991,
-        -0.090071993,
-        -0.072057594,
-        -0.057646075,
-        -0.04611686,
-    ]
-)
-y_vals = np.array(
-    [
-        16,
-        10.24,
-        6.5536,
-        4.194304,
-        2.68435456,
-        1.717986918,
-        1.099511628,
-        0.703687442,
-        0.450359963,
-        0.288230376,
-        0.184467441,
-        0.118059162,
-        0.075557864,
-        0.048357033,
-        0.030948501,
-        0.019807041,
-        0.012676506,
-        0.008112964,
-        0.005192297,
-        0.00332307,
-        0.002126765,
-    ]
-)
+# Current values at starting point we chose:
+x_large_step_current = x_start
+y_large_step_current = y_start
+dy_dx_current = dy_dx(x_large_step_current)
 
-plt.scatter(x_vals, y_vals)
+# To keep track of our coordinates at each step, we will create 2 lists, initialized with the values at our chosen starting point
+x_large_coords = [x_large_step_current]
+y_large_coords = [y_large_step_current]
 
-plt.xlim(xmin=-5, xmax=5)
-plt.ylim(ymin=0, ymax=25)
-plt.show()
-# -
+# New step_size
+large_step_size = 1.5
 
-# This works, but it takes a long time since we have several small steps. We could speed up the process by taking large steps. Starting at (-4,16) and increasing the step of size to $1.5$ we find:
+# Get coordinates of our second point using x_new equation and y = x ** 2
+x_large_step_new = x_large_step_current - dy_dx(x_large_step_current) * large_step_size
+y_large_step_new = y(x_large_step_new)
 
-# +
-q = np.linspace(-20, 20, 1000)
-plt.plot(q, q ** 2)
+# Store second point coordinates in our lists
+x_large_coords.append(x_large_step_new)
+y_large_coords.append(y_large_step_new)
 
 
-x_vals = np.array([-4, 8])
-y_vals = np.array([16, 64])
-
-plt.plot(x_vals, y_vals)
+plt.plot(x_large_coords, y_large_coords)  # plot points showing steps
 
 
 plt.xlim(xmin=-20, xmax=20)
@@ -264,44 +266,60 @@ plt.show()
 # *Note the change in scale.* With only one step, we already see that we stepped over the minimum! If we look at the tangent line at this point we see that the direction of the steepest descent is now to the left instead of the right:
 
 # +
-q = np.linspace(-20, 20, 1000)
-plt.plot(q, q ** 2)
+x_large_step = np.linspace(-20, 20, 1000)
+plt.plot(x_large_step, y(x_large_step))  # plot y = x ** 2
 
 
-plt.scatter(-4, 16)  # point
-qp = -4
-yp = 16
+plt.plot(x_large_coords, y_large_coords)  # plot points showing steps
 
 
-plt.scatter(8, 64)  # second point
-qp2 = 8
-yp2 = 64
-
-
-plt.plot(q, qp2 * 2 * (q - qp2) + yp2)  # tangent line
+plt.plot(
+    x_large_step,
+    dy_dx(x_large_step_new) * (x_large_step - x_large_step_new) + y_large_step_new,
+)  # tangent line
 
 plt.xlim(xmin=-20, xmax=20)
 plt.ylim(ymin=0, ymax=260)
 plt.show()
 # -
 
-# In our attempt to continue to find the minimum, we would now take a step of the same size, but now to the left. This puts us back at on the left side of the hill, but now above where we started. We would be stuck going back and forth between the two sides of the hill continuing to go upward.
-
-# We know as we get closer to the minimum the slope/gradient levels out. This means if we are very far from the minimum, we know we can take a very large step without stepping over the minimum. However, if we are close to the minimum, with a very small slope, we need to be careful and take smaller steps.
-#
-# We would continue this process of (1) Calculating our slope, (2) Determining a step size, and (3) Moving that step size until we finally reached a spot where the slope or gradient is essentially zero.
-#
-# This process is a gradient descent and it is used to minimize functions.
+# In our attempt to continue to find the minimum, we would take a step of the same size, but now to the left. This puts us back on the left side of the hill, but now above where we started. We would be stuck going back and forth between the two sides of the hill continuing to go upward.
 
 # +
-q = np.linspace(-20, 20, 1000)
-plt.plot(q, q ** 2)
+x_large_step = np.linspace(-20, 20, 1000)
+plt.plot(x_large_step, y(x_large_step))  # plot y = x ** 2
+
+# We are now at our second point so we need to update our current values
+x_large_step_current = x_large_step_new
+y_large_step_current = y_large_step_new
 
 
-x_vals = np.array([-4, 8, -16])
-y_vals = np.array([16, 64, 256])
+# We automate this process with the following while loop
 
-plt.plot(x_vals, y_vals)
+num_iter = 0  # To keep track of number of steps
+while abs(dy_dx(x_large_step_current)) > 0.1:  # Check to see if we're at minimum
+    # Get new coordinates
+    x_large_step_new = (
+        x_large_step_current - dy_dx(x_large_step_current) * large_step_size
+    )
+    y_large_step_new = y(x_large_step_new)
+
+    # Add new coordinates to lists
+    x_large_coords.append(x_large_step_new)
+    y_large_coords.append(y_large_step_new)
+
+    # Update current position
+    x_large_step_current = x_large_step_new
+
+    # Update number of iterations
+    num_iter = num_iter + 1
+
+    # Break loop if minimum not found within 20 steps
+    if num_iter > 20:
+        break
+
+
+plt.plot(x_large_coords, y_large_coords)  # plot points showing steps
 
 
 plt.xlim(xmin=-20, xmax=20)
@@ -310,10 +328,18 @@ plt.show()
 # -
 
 #
-# This is why it is important to pick the proper step size- also known as the learning rate. Too small steps take a long time while steps that are too large may cause us to miss the minimum. We should pick a step size that is in between the two. For example, in this case a reasonable choice would have been `step size = 0.6`, as it would have approximately reached the minimum after 3 steps.
+# This is why it is important to pick the proper step size- also known as the learning rate. Steps that are too small take a long time while steps that are too large may cause us to miss the minimum. We should pick a step size that is in between the two. For example, in this case a reasonable choice would have been <code> step size = 0.6 </code>, as it would have approximately reached the minimum after 3 steps.
 #
 #
-# This process of: (1)Calculating the gradient at a point, (2) Determining if the gradient is within the stopping criterion (in this case, the gradient is about equal to zero or $<0.01$), and (3) Taking a step if the criterion is not met, is known as Gradient Descent.
+# This process of:
+#
+# --- Calculating the gradient at a point
+#
+# --- Determining if the gradient is within the stopping criterion (in this case, the gradient is about equal to zero or $<0.1$)
+#
+# --- Taking a step if the criterion is not met
+#
+#  is known as Gradient Descent.
 #
 # ## Additional Resources
 #
