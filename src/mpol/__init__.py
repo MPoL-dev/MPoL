174f3ac3baa66afdf3ffb98f4a6d20e69191d3f7
Jeff Jennings <jjennings1519@gmail.com>
2023-02-02 11:17:35 -0500

    update __init__ to enable internal logging

--- src/mpol/__init__.py
+++ src/mpol/__init__.py
@@ -1,1 +1,24 @@
 __version__ = "0.1.13"
+
+def enable_logging(log_file=None):
+    """Turn on internal logging for MPoL
+
+    Parameters
+    ----------
+    log_file : string, optional
+        Output filename to which logging messages are written.
+        If not provided, logs will only be printed to the screen
+    """
+    import logging
+
+    if log_file is not None:
+        handlers = [ logging.FileHandler(log_file, mode='w'),
+                     logging.StreamHandler()
+                     ]
+    else:
+        handlers = [ logging.StreamHandler() ]
+
+    logging.basicConfig(level=logging.INFO,
+                        format='%(message)s',
+                        handlers=handlers
+                        )
