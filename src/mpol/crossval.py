3558e4addbd108591eb1a23eb568694ef741f03a
Jeff Jennings <jjennings1519@gmail.com>
2023-02-01 23:58:28 -0500

    CrossValidate: add split_dataset wrapper func

--- src/mpol/crossval.py
+++ src/mpol/crossval.py
@@ -14,3 +14,23 @@ class CrossValidate:
         self._gridder = gridder
         self._config = config
         self._verbose = verbose
+
+
+    def split_dataset(self, dataset):
+        r"""
+        # TODO
+        """
+        # create a radial and azimuthal partition for the dataset
+        dartboard = Dartboard(coords=self._coords)
+
+        # use 'dartboard' to split full dataset into train/test subsets
+        subsets = KFoldCrossValidatorGridded(dataset, self._config["kfolds"],
+                                        dartboard=dartboard,
+                                        npseed=self._config["seed"])#, device=device) # TODO
+
+        # store the individual train/test subsets
+        test_train_datasets = [(train_pair, test_pair) for (train_pair, test_pair) in subsets]
+        # test_train_datasets = [(train.to('cuda'), test.to('cuda')) for (train, test) in subsets] # TODO
+
+        return test_train_datasets
+
