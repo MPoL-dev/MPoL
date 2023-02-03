b2373565ee810f4516b1bd954301d0ea5698f81f
Jeff Jennings <jjennings1519@gmail.com>
2023-02-03 16:07:38 -0500

    crossval_test: add uniform cell splitter test

--- test/crossval_test.py
+++ test/crossval_test.py
@@ -1,11 +1,22 @@
 from mpol.crossval import CrossValidate
 
 
-def test_crossvalclass_split(coords, gridder, dataset, generic_parameters):
-    # using the CrossValidate class, split a dataset into train/test subsets
+def test_crossvalclass_split_dartboard(coords, gridder, dataset, generic_parameters):
+    # using the CrossValidate class, split a dataset into train/test subsets 
+    # using 'dartboard' splitter
 
     crossval_pars = generic_parameters["crossval_pars"]
+    crossval_pars["split_method"] = "dartboard"
 
+    cross_validator = CrossValidate(coords, gridder, **crossval_pars)
+    test_train_datasets = cross_validator.split_dataset(dataset)
+
+
+def test_crossvalclass_split_uniformcell(coords, gridder, dataset, generic_parameters):
+    # using the CrossValidate class, split a dataset into train/test subsets 
+    # using 'dartboard' splitter
+
+    crossval_pars = generic_parameters["crossval_pars"]
     cross_validator = CrossValidate(coords, gridder, **crossval_pars)
     test_train_datasets = cross_validator.split_dataset(dataset)
 
