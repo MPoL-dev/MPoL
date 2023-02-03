257ac957c94c3f6abf9371b682d9b9cdd549637a
Jeff Jennings <jjennings1519@gmail.com>
2023-02-03 01:11:18 -0500

    TrainTest: update init par

--- src/mpol/train_test.py
+++ src/mpol/train_test.py
@@ -21,7 +21,7 @@ class TrainTest:
     """
 
     def __init__(self, gridder, optimizer, epochs=500, convergence_tol=1e-2, 
-                lambda_guess_regularizers=[""], lambda_entropy=None, 
+                lambda_guess=None, lambda_entropy=None, 
                 entropy_prior_intensity=None, lambda_sparsity=None, lambda_TV=None, 
                 TV_epsilon=1e-10, lambda_TSV=None, 
                 train_diag_step=None, diag_fig_train=False, verbose=True):
@@ -29,7 +29,7 @@ class TrainTest:
         self._optimizer = optimizer
         self._epochs = epochs
         self._convergence_tol = convergence_tol
-        self._lambda_guess_regularizers = lambda_guess_regularizers
+        self._lambda_guess = lambda_guess
         self._lambda_entropy = lambda_entropy
         self._entropy_prior_intensity = entropy_prior_intensity
         self._lambda_sparsity = lambda_sparsity
