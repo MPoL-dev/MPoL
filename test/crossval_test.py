6b4153ba900c05fd6ae339a4164a0417c1520ce6
Jeff Jennings <jjennings1519@gmail.com>
2023-02-03 13:52:06 -0500

    crossval_test: add TODO

--- test/crossval_test.py
+++ test/crossval_test.py
@@ -12,7 +12,7 @@ def test_crossvalclass_split(coords, gridder, dataset, generic_parameters):
 
 def test_crossvalclass_kfold(coords, gridder, dataset, generic_parameters):
     # using the CrossValidate class, perform k-fold cross-validation
-
+ 
     crossval_pars = generic_parameters["crossval_pars"]
     # reset some keys to bypass functionality tested elsewhere and speed up test
     crossval_pars["lambda_guess"] = None
@@ -21,3 +21,5 @@ def test_crossvalclass_kfold(coords, gridder, dataset, generic_parameters):
     cross_validator = CrossValidate(coords, gridder, **crossval_pars)
     test_train_datasets = cross_validator.split_dataset(dataset)
     cv_score, all_scores, loss_histories = cross_validator.run_crossval(test_train_datasets)
+
+# TODO: add gpu test
