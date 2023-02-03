cb6da0346fd61220d7296023c7989fff03a7d126
Jeff Jennings <jjennings1519@gmail.com>
2023-02-03 04:18:55 -0500

    crossval tests: speed up

--- test/crossval_test.py
+++ test/crossval_test.py
@@ -14,6 +14,9 @@ def test_crossvalclass_kfold(coords, gridder, dataset, generic_parameters):
     # using the CrossValidate class, perform k-fold cross-validation
 
     crossval_pars = generic_parameters["crossval_pars"]
+    # reset some keys to bypass functionality tested elsewhere and speed up test
+    crossval_pars["lambda_guess"] = None
+    crossval_pars["epochs"] = 11
 
     cross_validator = CrossValidate(coords, gridder, **crossval_pars)
     test_train_datasets = cross_validator.split_dataset(dataset)
