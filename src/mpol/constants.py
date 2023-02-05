39e2ee88dd143e783b1c95e70db4344f2699d54c
Kadri Nizam <knizam@psu.edu>
2023-02-04 19:08:23 -0500

    constants.py type hinted

--- src/mpol/constants.py
+++ src/mpol/constants.py
@@ -1,11 +1,18 @@
+from __future__ import annotations
+
+from typing import Final
+
+import astropy.constants as apy_const
 import numpy as np
-from astropy.constants import c, k_B
 
 # convert from arcseconds to radians
-arcsec = np.pi / (180.0 * 3600)  # [radians]  = 1/206265 radian/arcsec
+arcsec: Final[float] = np.pi / (180.0 * 3600)  # [radians]  = 1/206265 radian/arcsec
 
-deg = np.pi / 180  # [radians]
+deg: Final[float] = np.pi / 180  # [radians]
 
-kB = k_B.cgs.value  # [erg K^-1] Boltzmann constant
-cc = c.cgs.value # [cm s^-1]
-c_ms = c.value # [m s^-1]
+# Boltzmann constant
+kB: Final[float] = getattr(apy_const, "k_B").cgs.value  # [erg K^-1]
+
+# Light speed
+cc: Final[float] = getattr(apy_const, "c").cgs.value  # [cm s^-1]
+c_ms: Final[float] = getattr(apy_const, "c").value  # [m s^-1]
