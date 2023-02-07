8875cd652c721f60a2d1e31a4b81f9ad77dfb609
Jeff Jennings <jjennings1519@gmail.com>
2023-02-07 16:35:43 -0500

    update crossval test args

--- test/crossval_test.py
+++ test/crossval_test.py
@@ -10,16 +10,16 @@ def test_crossvalclass_split_dartboard(coords, gridder, dataset, generic_paramet
     crossval_pars["split_method"] = "dartboard"
 
     cross_validator = CrossValidate(coords, gridder, **crossval_pars)
-    test_train_datasets = cross_validator.split_dataset(dataset)
+    cross_validator.split_dataset(dataset)
 
 
 def test_crossvalclass_split_randomcell(coords, gridder, dataset, generic_parameters):
     # using the CrossValidate class, split a dataset into train/test subsets 
-    # using 'dartboard' splitter
+    # using 'random_cell' splitter
 
     crossval_pars = generic_parameters["crossval_pars"]
     cross_validator = CrossValidate(coords, gridder, **crossval_pars)
-    test_train_datasets = cross_validator.split_dataset(dataset)
+    cross_validator.split_dataset(dataset)
 
 
 def test_crossvalclass_kfold(coords, gridder, dataset, generic_parameters):
@@ -31,6 +31,7 @@ def test_crossvalclass_kfold(coords, gridder, dataset, generic_parameters):
     crossval_pars["epochs"] = 11
 
     cross_validator = CrossValidate(coords, gridder, **crossval_pars)
+    cv_score, all_scores, loss_histories = cross_validator.run_crossval(dataset)
 
 
 def test_dartboardsplit_init(crossvalidation_products):
