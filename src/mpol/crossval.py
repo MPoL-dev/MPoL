5947598d6b8b06a8345c9c2328dafaf8baf7fcc4
Jeff Jennings <jjennings1519@gmail.com>
2023-02-09 02:24:48 -0500

    crossval.py: import TODO

--- src/mpol/crossval.py
+++ src/mpol/crossval.py
@@ -12,7 +12,7 @@ import torch
 from mpol.precomposed import SimpleNet
 from mpol.training import TrainTest
 from mpol.datasets import Dartboard, GriddedDataset
-from mpol.plot import splitter_diagnostics_fig
+# from mpol.plot import splitter_diagnostics_fig # TODO
 
 
 class CrossValidate:
