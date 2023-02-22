e6be9e6b7113466df63a790a5a9d3ac52cd5c0fb
Jeff Jennings <jjennings1519@gmail.com>
2023-02-22 15:32:59 -0500

    CrossValidate: add model to self

--- src/mpol/crossval.py
+++ src/mpol/crossval.py
@@ -198,11 +198,11 @@ class CrossValidate:
             #     train_set, test_set = train_set.to(self._device), test_set.to(self._device)
 
             # create a new model and optimizer for this k_fold
-            model = SimpleNet(coords=self._coords, nchan=self._imager.nchan)
+            self.model = SimpleNet(coords=self._coords, nchan=self._imager.nchan)
             # if hasattr(self._device,'type') and self._device.type == 'cuda': # TODO: confirm which objects need to be passed to gpu
-            #     model = model.to(self._device)
+            #     self.model = self.model.to(self._device)
 
-            optimizer = torch.optim.Adam(model.parameters(), lr=self._learn_rate)
+            optimizer = torch.optim.Adam(self.model.parameters(), lr=self._learn_rate)
 
             trainer = TrainTest(
                 imager=self._imager,
@@ -223,10 +223,10 @@ class CrossValidate:
                 verbose=self._verbose,
             )
 
-            loss, loss_history = trainer.train(model, train_set)
+            loss, loss_history = trainer.train(self.model, train_set)
             if self._store_cv_diagnostics:
                 self._diagnostics["loss_histories"].append(loss_history)
-            all_scores.append(trainer.test(model, test_set))
+            all_scores.append(trainer.test(self.model, test_set))
             # store the most recent train figure for diagnostics
             self.train_figure = trainer.train_figure 
 
