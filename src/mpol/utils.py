916c3604ccb7d1d374aea3f5021334ab84c61168
Jeff Jennings <jjennings1519@gmail.com>
2023-03-13 17:05:25 -0400

    get_optimal_image_properties: add safety check

--- src/mpol/utils.py
+++ src/mpol/utils.py
@@ -324,6 +324,9 @@ def get_optimal_image_properties(image_width, u, v):
     if npix % 2 == 1:
         npix += 1
 
+    # should never occur 
+    assert(get_max_spatial_freq(cell_size, npix) >= max_freq), "error in get_optimal_image_properties"
+
     return cell_size, npix
 
 
