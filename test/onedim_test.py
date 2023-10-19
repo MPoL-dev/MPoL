f7efc90f48060faed7528dd6f311cf4c8579da83
Jeff Jennings <jjennings1519@gmail.com>
2023-10-19 14:00:58 -0400

    start of onedim tests

--- test/onedim_test.py
+++ test/onedim_test.py
@@ -1,4 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from mpol.coordinates import GridCoords
 from mpol.onedim import radialI, radialV
+from mpol.precomposed import SimpleNet
+from mpol.utils import sky_gaussian_arcsec
+
+def test_radialV(coords, imager, dataset, generic_parameters):
+    # obtain a 1d radial visibility profile V(q) from 2d visibilities
+    coords = GridCoords(cell_size=0.005, npix=800)
+
+    g = sky_gaussian_arcsec(coords.sky_x_centers_2D, coords.sky_y_centers_2D)
+    
+    nchan = dataset.nchan
+    model = SimpleNet(coords=coords, nchan=nchan)
+
+def test_radialI(coords, imager, dataset, generic_parameters):
+    # obtain a 1d radial brightness profile I(r) from an image
+    nchan = dataset.nchan
+    model = SimpleNet(coords=coords, nchan=nchan)
