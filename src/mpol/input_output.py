c0dcc1bc35045209f896740f5f0aeacb48801311
Jeff Jennings <jjennings1519@gmail.com>
2023-11-29 17:32:11 -0500

    input_output: new class: ProcessFitsImage

--- src/mpol/input_output.py
+++ src/mpol/input_output.py
@@ -2,3 +2,19 @@ import numpy as np
 
 from astropy.io import fits
 
+class ProcessFitsImage:
+    """
+    Utilities for loading and retrieving metrics of a .fits image
+
+    Parameters
+    ----------
+    filename : str
+        Path to the .fits file
+    channel : int, default=0
+        Channel of the image to access
+    """
+
+    def __init__(self, filename, channel=0):
+        self._fits_file = filename
+        self._channel = channel
+
