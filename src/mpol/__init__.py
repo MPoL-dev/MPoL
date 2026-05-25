ab7f9f982be07819235db0fff37783c1ec82b249
Ian Czekala <iancze@gmail.com>
2026-05-25 15:32:01 +1000

    move module import to top of file.

--- src/mpol/__init__.py
+++ src/mpol/__init__.py
@@ -1,8 +1,8 @@
-zenodo_record = 10064221
-
 import importlib.metadata
 
 try:
     __version__ = importlib.metadata.version("MPoL")
 except importlib.metadata.PackageNotFoundError:
     __version__ = "unknown"
+
+zenodo_record = 10064221
